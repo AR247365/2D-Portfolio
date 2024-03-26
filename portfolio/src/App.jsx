e7a9a4ec89f52ac8fddc0e98425b42713f4dd712
AR247365 <lonewolf22081998@gmail.com>
2024-03-27 02:46:27 +0530

    hero

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,31 +1,7 @@
+import Portfolio from "./Portfolio"
+
 function App() {
-  return (
-    <>
-      <main className="px-10 py-5 ">
-        <h1 className="font-black text-8xl py-4 ">FRONT-END DEVELOPER</h1>
-        <h2 className="font-bold text-7xl py-4 underline">PROJECTS</h2>
-        <br />
-        <ul className="">
-          <li className="font-medium text-5xl py-7">
-            <a
-              href=""
-              className="border-8 px-3 border-black hover:bg-black hover:text-white"
-            >
-              PRODUCT LAUNCH
-            </a>
-          </li>
-          <li className="font-medium text-5xl py-7 ">
-            <a
-              href=""
-              className="border-8 px-3 border-black hover:bg-black hover:text-white"
-            >
-              ART EXHIBITION
-            </a>
-          </li>
-        </ul>
-      </main>
-    </>
-  )
+  return <Portfolio />
 }
 
 export default App
---
+++ portfolio/src/Portfolio.jsx
@@ -0,0 +1,113 @@
+export default function Portfolio() {
+  return (
+    <>
+      <main className="px-10 py-5 bg-black">
+        <h1 className="text-white font-bold text-7xl py-4 text-center">
+          FRONT-END DEVELOPER
+        </h1>
+        <h2 className="text-white font-semibold text-6xl py-4 underline text-center">
+          PROJECTS
+        </h2>
+        <br />
+        <ul>
+          <li className=" py-7 text-center">
+            <a
+              target="_blank"
+              href=""
+              className="text-white font-normal text-5xl border-4 px-3 border-white hover:bg-white hover:text-black "
+            >
+              PRODUCT LAUNCH
+            </a>
+          </li>
+          <li className=" py-7 text-center ">
+            <a
+              href=""
+              target="_blank"
+              className="text-white font-normal text-5xl border-4 px-3 border-white hover:bg-white hover:text-black"
+            >
+              ART EXHIBITION
+            </a>
+          </li>
+          <li className=" py-7 text-center">
+            <a
+              href="https://marble-race-3d.netlify.app"
+              target="_blank"
+              className="text-white font-normal text-5xl border-4 px-3 border-white hover:bg-white hover:text-black"
+            >
+              MARBLE RACE
+            </a>
+          </li>
+          <li className=" py-7 text-center">
+            <a
+              href="https://e-commerce-web-site-comfy.netlify.app/"
+              target="_blank"
+              className="text-white font-normal text-5xl border-4 px-3 border-white hover:bg-white hover:text-black"
+            >
+              E-COMMERCE
+            </a>
+          </li>
+          <li className=" py-7 text-center">
+            <a
+              href="https://j0b-tracker.netlify.app/"
+              target="_blank"
+              className="text-white font-normal text-5xl border-4 px-3 border-white hover:bg-white hover:text-black"
+            >
+              JOB LEDGER
+            </a>
+          </li>
+          <li className=" py-7 text-center ">
+            <a
+              href="https://galaxy-creator-3142857.netlify.app/"
+              target="_blank"
+              className="text-white font-normal text-5xl border-4 px-3 border-white hover:bg-white hover:text-black"
+            >
+              PRESS "H"
+            </a>
+          </li>
+          <li className=" py-7 text-center ">
+            <a
+              href="https://blender-scene.netlify.app/"
+              target="_blank"
+              className="text-white font-normal text-5xl border-4 px-3 border-white hover:bg-white hover:text-black"
+            >
+              PORTAL SCENE
+            </a>
+          </li>
+          <li className=" py-7 text-center ">
+            <a
+              href="https://time-piece.netlify.app/"
+              target="_blank"
+              className="text-white font-normal text-5xl border-4 px-3 border-white hover:bg-white hover:text-black"
+            >
+              PILOT
+            </a>
+          </li>
+        </ul>
+        <h2 className="text-white font-semibold text-6xl py-4 underline text-center">
+          CONTACT
+        </h2>
+        <ul>
+          <li className=" py-7 text-center ">
+            <a
+              href="https://www.linkedin.com/in/abdul-rahaman-b3b465202"
+              target="_blank"
+              className="text-white font-normal text-5xl border-4 px-3 border-white hover:bg-white hover:text-black"
+            >
+              LINKEDIN
+            </a>
+          </li>
+          <li className=" py-7 text-center ">
+            <span className="text-white font-normal text-5xl border-4 px-3 border-white hover:bg-white hover:text-black">
+              +91 8871570611
+            </span>
+          </li>
+          <li className=" py-7 text-center ">
+            <span className="text-white font-normal text-5xl border-4 px-3 border-white hover:bg-white hover:text-black">
+              abdulrahaman220898@gmail.com
+            </span>
+          </li>
+        </ul>
+      </main>
+    </>
+  )
+}
