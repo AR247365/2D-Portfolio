c20fc01f9d90a732bc08613e2c5545de491b2b12
AR247365 <lonewolf22081998@gmail.com>
2024-04-05 00:23:04 +0530

    Final

--- portfolio/src/Portfolio.jsx
+++ portfolio/src/Portfolio.jsx
@@ -13,7 +13,7 @@ export default function Portfolio() {
           <li className=" py-7 text-center">
             <a
               target="_blank"
-              href=""
+              href="https://product-launch-camera.netlify.app/"
               className="text-white font-normal text-5xl border-4 px-3 border-white hover:bg-white hover:text-black "
             >
               PRODUCT LAUNCH
@@ -21,7 +21,7 @@ export default function Portfolio() {
           </li>
           <li className=" py-7 text-center ">
             <a
-              href=""
+              href="https://art-three-graces.netlify.app/"
               target="_blank"
               className="text-white font-normal text-5xl border-4 px-3 border-white hover:bg-white hover:text-black"
             >
